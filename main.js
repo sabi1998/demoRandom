60a08acf6a09acd50a1798eecad81039321ea1d0
Sabi <31340198+sabi1998@users.noreply.github.com>
2022-06-12 17:22:56 +0545

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function calc(p,r,m){
     arr =[];
     var tmpA = 0;
     for(var x =0; x < m; x++){
-    	tmpA = parseFloat(tmpA).toFixed(2) + parseFloat((p * parseFloat(r / m).toFixed(2)) / 100).toFixed(2);
+    	tmpA = parseFloat((parseFloat(tmpA + p).toFixed(2) * parseFloat(r / m).toFixed(2)) / 100).toFixed(2);
         arr.push(parseFloat(tmpA).toFixed(2));
     }
 }
